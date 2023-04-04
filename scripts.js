042cd182ac2ff7860fd2281f2bca6257bf068308
sepowell <seanp1998@gmail.com>
2023-04-04 12:38:39 -0700

    Add skeleton to title and form areas
    Also includes basic styling

--- scripts.js
+++ scripts.js
@@ -4,9 +4,8 @@ let myLibrary = [];
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author; 
-  this.pages = pages;
   this.read = read;
   this.info = functiom() {
-    return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`;
+    return `${this.title} by ${this.author}, ${this.read}`;
   }
 }

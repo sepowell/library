99deb0fb0b776974dca53de07b19fe7c32c2d13a
sepowell <seanp1998@gmail.com>
2023-04-04 21:14:51 -0700

    Register inputs into Library array
    also performs a basic render of the book title to establish a working connection

--- scripts.js
+++ scripts.js
@@ -1,11 +1,32 @@
 let myLibrary = [];
 
+let submitNewBookBtn = document.getElementById('submit-book');
+
+submitNewBookBtn.addEventListener('click', addBookToLibrary);
+
 // Book object constructor
-function Book(title, author, pages, read) {
+function Book(title, author, read) {
   this.title = title;
   this.author = author; 
   this.read = read;
-  this.info = functiom() {
-    return `${this.title} by ${this.author}, ${this.read}`;
+}
+
+function addBookToLibrary() {
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+  let status = document.querySelector('#status').value;
+
+  let newBook = new Book(title, author, status);
+  myLibrary.push(newBook);
+  render();
+}
+
+function render() {
+  let libraryContainer = document.querySelector('.library-grid');
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let bookEl = document.createElement('div');
+    bookEl.innerHTML = `<p>${book.title}</p>`;
+    libraryContainer.appendChild(bookEl);
   }
 }

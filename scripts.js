b3ccb73947b4650faef64129d1574e7e694df0c8
sepowell <seanp1998@gmail.com>
2023-04-05 12:40:48 -0700

    Add styling to created book containers

--- scripts.js
+++ scripts.js
@@ -14,19 +14,24 @@ function Book(title, author, read) {
 function addBookToLibrary() {
   let title = document.querySelector('#title').value;
   let author = document.querySelector('#author').value;
-  let status = document.querySelector('#status').value;
+  let read = document.querySelector('#status').value;
 
-  let newBook = new Book(title, author, status);
+  let newBook = new Book(title, author, read);
   myLibrary.push(newBook);
   render();
 }
 
 function render() {
   let libraryContainer = document.querySelector('.library-grid');
+  libraryContainer.innerHTML = '';
   for (let i = 0; i < myLibrary.length; i++) {
     let book = myLibrary[i];
     let bookEl = document.createElement('div');
-    bookEl.innerHTML = `<p>${book.title}</p>`;
+    bookEl.classList.add('book-container');
+    bookEl.innerHTML = `
+      <h2 class="book-title">${book.title}</h2>
+      <p class="book-author">${book.author}</p>
+      <p class="status">${book.read}</p>`;
     libraryContainer.appendChild(bookEl);
   }
 }
